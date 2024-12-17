23a2d5b0d7205b8e2f48b90a39be0dfecaca521f
monica <monicaafriend@gmail.com>
2024-12-17 11:18:55 -0600

    adds array functions

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats, name];
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [name, ...cats];
+    return copyOfCats;
+}
+
+function removeLastCat() {
+    const copyOfCats = cats.slice(0, cats.length - 1);
+    return copyOfCats;
+}
+
+function removeFirstCat() {
+    const copyOfCats = cats.slice(1);
+    return copyOfCats;
+}
